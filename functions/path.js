b4bdc2d49ee54df4278cf6547fe4a16057b12f2d
Mariana Arenas <arenasm8@hotmail.com>
2023-03-08 22:31:02 -0300

    agregué funciones de los módulos de node en utils.js, falta manejarlos con condicionales

--- functions/path.js
+++ functions/path.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const fs = require('fs')
 
+//transformando ruta relativa a absoluta
 let route = path.resolve('./files')
 console.log('route: ', route)
 
+//función para validar path
+const pathValidate = (entryPath) => fs.existsSync(entryPath)
+console.log('pathValidate: ', pathValidate('./files'))
+
+//guardar ruta de archivo
 let fileRoute = path.resolve('./files/first-file.md')
 
 //leyendo directorio
--- functions/utils.js
+++ functions/utils.js
@@ -1,11 +1,51 @@
 const path = require('path')
 const fs = require('fs')
 
+//función para validar que existe el path, devuelve true o false
+const pathValidate = (entryPath) => fs.existsSync(entryPath)
+console.log('pathValidate: ', pathValidate('./files'))
+
+//función para saber si la ruta es absoluta, devuelve true o false
 const isAnAbsolutePath = (entryPath) => path.isAbsolute(entryPath);
-if(isAnAbsolutePath('./files')){
-    console.log('es un path absoluto')
+console.log('isAnAbsolutePath: ', isAnAbsolutePath('./files'))
+
+//función para hacer absoluta una ruta relativa
+const pathResolve = (entryPath) => path.resolve(entryPath)
+console.log('pathResolve: ', pathResolve('./files'))
+
+//función para saber si es un directorio, devuelve true o false
+const isDirectory = (entryPath) => {
+    const stats = fs.statSync(entryPath);
+    return stats.isDirectory()
 }
-else {
-    //ejecutar función para hacerla absoluta
-    console.log('es un path relativo')
+console.log('isDirectory: ', isDirectory('./files/first-file.md'))
+
+//función para saber si es un archivo, devuelve true o false
+const isFile = (entryPath) => {
+    const stats = fs.statSync(entryPath);
+    return stats.isFile()
 }
+console.log('isFile: ', isFile('./files/first-file.md'))
+
+//función para obtener la extensión
+const getExtFile = (entryPath) => path.extname(entryPath)
+console.log('getExtFile: ', getExtFile('./files/first-file.md'))
+
+//función para leer un directorio
+const readDir = (entryPath) => fs.readdirSync(entryPath, 'utf8')
+console.log('readDir: ', readDir('./files') )
+
+//función para leer el contenido del archivo
+const readFile = (entryPath) => fs.readFile(entryPath, 'utf8', (err, data) => {
+    if(err) throw err;
+    console.log('contenido archivo readFile: ', data)
+})
+console.log('readFile: ', readFile('./files/firs-file.md'), ' por qué devuelve undefined?')
+
+// if(isAnAbsolutePath('./files')){
+//     console.log('es un path absoluto')
+// }
+// else {
+//     pathResolve('./files')
+//     console.log('es un path relativo')
+// }
