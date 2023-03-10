a102e08efb33b76bf22e865ac41f1a0304f2924f
Mariana Arenas <arenasm8@hotmail.com>
2023-03-10 17:53:22 -0300

    hasta ahora lee los links, pero todavía no separa texto de link

--- files/first-file.md
+++ files/first-file.md
@@ -1,8 +1,8 @@
+## 1. Preámbulo
+
 "hello world from first file"
-(https://es.wikipedia.org/wiki/Markdown)
-(https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
-(https://es.wikipedia.org/wiki/Markdown)
-<!-- ## 1. Preámbulo
+[Markdown](https://es.wikipedia.org/wiki/Markdown)
+[ahora deberia tomar](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
 
 [Markdown](https://es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado
 ...
@@ -15,4 +15,4 @@ herramienta usando [Node.js](https://nodejs.org/), que lea y analice archivos
 en formato `Markdown`, para verificar los links que contengan y reportar
 algunas estadísticas.
 
-![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg) -->
+![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
--- functions/practice_mat.js
+++ functions/practice_mat.js
@@ -4,11 +4,11 @@ const fs = require('fs')
 // const fileRoute = path.resolve('./files/first-file.md')
 // console.log('file route: ', fileRoute)
 
-const readFile = (entryPath) => fs.readFileSync(entryPath, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log('contenido archivo readFile: ', data)
-    return data
-})
+// const readFile = (entryPath) => fs.readFileSync(entryPath, 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log('contenido archivo readFile: ', data)
+//     return data
+// })
 
 // const gettingLinks = (entryPath) => {
 //     readFile(entryPath)
@@ -25,15 +25,27 @@ const readFile = (entryPath) => fs.readFileSync(entryPath, 'utf8', (err, data) =
 
 // console.log('getting links: ', gettingLinks(fileRoute))
 
-const regExp = /\[([\w\s\d]+)\]\(((?:\/|https?:\/\/)[\w\d./?=#]+[a-zA-Z0-9!-_$]+)\)/gi;
+const regExp = /\[([^\[]+)\](\(.*\))/gm;
+//const regExp = /^\[([\w\s\d]+)\]\((https?:\/\/[\w\d./?=#]+)\)$/gm
 const file = path.resolve('./files/first-file.md')
-const content = fs.readFileSync(file)
-const getLinks = regExp.exec(content);
-if (getLinks !== null){
-    const arrayContent = [];
-    getLinks.forEach(element => {
-        arrayContent.push(getLinks[0])
-        console.log(arrayContent)
-    });
+const content = fs.readFileSync(file, 'utf8').toString()
+// console.log({content})
+const getLinks = content.match(regExp);
+const arrayContent = [];
 
-}
+if (getLinks !== null){        
+    getLinks.forEach(element => {
+        //console.log({element})
+        // const links = regExp.exec(element)
+        // console.log('links: ', links)       
+        arrayContent.push(element)        
+        // return arrayContent
+        
+    });
+    // console.log('getLinks: ', getLinks)
+     console.log('array content:', arrayContent)     
+    
+};
+
+
+
